c74489f20497942e32f8530705c4ee429a7919c4
Rahil <RahilQayoom.dev@gmail.com>
2025-10-25 17:34:00 +0530

    Level: i was stuck as to how to map the char to dict in which google helped me and also doing wrong because th increamnet i needed the not the global num and i also didnt need split and two nested loops i still have lot to do

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from stats import get_word_count, get_book_text
+from stats import get_word_count, get_book_text , get_char_count
 
 
 def main():
     counted_words = get_word_count(get_book_text("//home//zen//Desktop//Boot-dev_Projects///bookbot//books//frankenstein.txt"))
     print(f"Found {counted_words} total words")
+    char_counts = get_char_count(get_book_text("//home//zen//Desktop//Boot-dev_Projects///bookbot//books//frankenstein.txt"))
+    print(char_counts)
+
+    ##get_char_count("Hello World!, This This world ")
 
 main()
--- stats.py
+++ stats.py
@@ -3,6 +3,35 @@ def get_book_text(path_to_file):
         file_contents = f.read()
         return file_contents
 
+# def get_char_count(words_from_book):
+#     char_count = dict()
+#     ##num = 0
+#     ## words_from_book = words_from_book.lower().split()
+#     words_from_book = words_from_book.lower()
+#     for j in words_from_book:
+#         ##num = 0
+#         for i in j:
+#             if i in char_count:
+#                 char_count[i] = char_count.get(i) + 1
+#             else:
+#                 char_count[i] = 1
+        
+    
+            
+def get_char_count(words_from_book):
+    char_count = dict()
+    for char in words_from_book.lower():
+        char_count[char] = char_count.get(char, 0) + 1     
+    return char_count
+
+
+# from boots more elegant and right solution 
+# def get_char_count(text):
+#     counts = {}
+#     for c in text.lower():
+#         counts[c] = counts.get(c, 0) + 1
+#     return counts
+
 def get_word_count(file_content):
     book_text = file_content.split()
     total_count:int = 0
